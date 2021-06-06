c2b60b8afa1cf377f1314f73b0e31eeb37c56af6
Xiong@Raspi <xiong3219@icloud.com>
2021-06-06 13:03:30 +0800

    add re.sh

---
+++ re.sh
@@ -0,0 +1,7 @@
+sudo apt-get install build-essential 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-gi-cairo
+sudo apt-get install lm-sensors
+pip3 install numpy -U --user
+pip3 install matplotlib --user
